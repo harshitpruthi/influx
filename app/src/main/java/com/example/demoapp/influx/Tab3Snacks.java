39bd477f62958ee91eff0c442ddb2a710bacbba6
HarshitP <harshitpruthi23@gmail.com>
2018-08-13 11:37:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demoapp/influx/Tab3Snacks.java
@@ -0,0 +1,21 @@
+package com.example.demoapp.influx;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HarshitP on 02-07-2018.
+ */
+
+public class Tab3Snacks extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3snacks, container, false);
+        return rootView;
+    }
+}
